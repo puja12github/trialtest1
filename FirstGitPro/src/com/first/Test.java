1483d57bd0e706924fd8a235e2d5a6d8062646b8
ADMIN <ADMIN@DESKTOP-EDT2IR2>
2022-10-12 22:19:39 +0530

    array define

--- FirstGitPro/src/com/first/Test.java
+++ FirstGitPro/src/com/first/Test.java
@@ -3,6 +3,11 @@ package com.first;
 public class Test {
 
 	public static void main(String[] args) {
+		int a[] = new int[]{1 ,2, 5 ,4 ,5};
+		for(int i =0 ; i<a.length ; i++)
+		{
+			System.out.print(a[i] + " ");
+		}
 
 	}
 
